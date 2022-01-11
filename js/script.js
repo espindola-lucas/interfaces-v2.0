550c774112f7395edde45c6ae4352e90cf2cae7f
RosarioTG <ro.tani.gomez@gmail.com>
2022-01-11 08:31:56 -0300

    Funcionalidad para ponerle like a las publicaciones

---
+++ js/script.js
@@ -0,0 +1,26 @@
+function like1() {
+    let span = "<span  style='color:red' id='like' class='fas fa-heart'></span> "
+        document.getElementById("P1").innerHTML =   span   +  665 ;
+    
+    }
+    
+    function like2() {
+        let span = "<span  style='color:red' id='like' class='fas fa-heart'></span> "
+            document.getElementById("P2").innerHTML =   span   +  11 ;
+    }
+    
+    function like3() {
+        let span = "<span  style='color:red' id='like' class='fas fa-heart'></span> "
+           document.getElementById("P3").innerHTML =   span   +  3 ;
+    }
+    
+    function like4() {
+        let span = "<span  style='color:red' id='like' class='fas fa-heart'></span> "
+           document.getElementById("P4").innerHTML =   span   +  91 ;
+    }
+    
+    function like5() {
+        let span = "<span  style='color:red' id='like' class='fas fa-heart'></span> "
+           document.getElementById("P5").innerHTML =   span   +  302 ;
+    }
+    
