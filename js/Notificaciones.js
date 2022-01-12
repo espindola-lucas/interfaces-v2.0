e131cb896adaefb6ceb6e792fdf61eb386e45759
RosarioTG <ro.tani.gomez@gmail.com>
2022-01-12 14:39:50 -0300

    Ver Notificacion

---
+++ js/Notificaciones.js
@@ -0,0 +1,16 @@
+
+    let click = 'si';
+function MostrarNotificaciones (){
+    if (click == 'si'){
+        document.getElementById('notificacion').style.display='block';
+        document.getElementById('campana').style.color='rgba(132, 75, 132, 1)';
+        campana
+        return   click = 'no';
+    }
+    if (click == 'no'){
+        document.getElementById('notificacion').style.display='none';
+        document.getElementById('campana').style.color='white';
+        return click = 'si';
+    }
+    
+}
