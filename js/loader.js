029bcdc7fe2c37987daae79f715edf18e5ed55fc
Lucas Nahuel Espindola <lucas.nahuel.espindola1998@gmail.com>
2022-01-31 10:01:18 -0300

    Agrego el overflow hidden al momento que carga el ring

--- js/loader.js
+++ js/loader.js
@@ -1,22 +1,20 @@
-
-
+"use strict"
 
 document.getElementById('loader').addEventListener("animationend", function () {
     document.getElementById('loader').style.display = "none";
     document.getElementById('ring').style.display = "none"; 
 });
 
-
-
+let n = 0;
 function Timer() {
-
-    let n = 0;
     let parar = 1;
     let entre = 0;
     
     window.setInterval(function(){
         if(parar != n){
             n++;
+            document.getElementById('loader').style.marginTop = '20%';
+            document.body.style.overflow = 'hidden';
         }else {
             if (entre == 0){
                 entre = 1 ;
@@ -24,11 +22,11 @@ function Timer() {
                 document.getElementById('ring').className = "ring-container-finish";
                 n = 0 ;
                 parar = 0;
+                document.body.style.overflow = 'scroll';
             }
             
         }
-    },1000);
-
+    },1500);
 }
 
 
