e6eddd86a9bd992c646b56938aaf3b1ef86205f2
RosarioTG <ro.tani.gomez@gmail.com>
2022-02-03 10:29:27 -0300

    iconos paraagregar con funcionalidad

---
+++ js/compartir.js
@@ -0,0 +1,8 @@
+function changeicon (){
+    document.getElementById("icono").className = "fas fa-user-clock";
+}
+function changeicon1(){
+    document.getElementById("icono1").className = "fas fa-user-clock";
+}
+
+
