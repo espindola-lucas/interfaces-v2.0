c444e6de89492d3c6f03b66a52666c6096fe6460
Lucas Nahuel Espindola <lucas.nahuel.espindola1998@gmail.com>
2022-02-07 14:57:30 -0300

    agrego un flujo para la publicidad, a medida que vas completando los datos, van apareciendo otros formularios

--- js/publicidad.js
+++ js/publicidad.js
@@ -1,12 +1,17 @@
 "use strict";
 
+let datosPublicidad = document.getElementById('datosPublicidad').style.display = 'none';
+let datosPago = document.getElementById('datosPago').style.display = 'none';
+let ultimoInputDatosPublicidad = document.getElementById('link');
+let ultimoInputDatosPago = document.getElementById('codigo'); 
+
 function prioridadBaja() {
         document.getElementById('prioridadMedia').style.border= "none"; 
         document.getElementById('prioridadAlta').style.border= "none"; 
         let prioridades = document.getElementById('prioridadBaja'); 
         prioridades.style.border = "1px solid #A66FA6";
         prioridades.style.borderRadius = '5px';
-    
+        document.getElementById('datosPublicidad').style.display = 'flex';
 }
 
 
@@ -16,8 +21,9 @@ function prioridadMedia() {
         let prioridades = document.getElementById('prioridadMedia'); 
         prioridades.style.border = "1px solid #A66FA6";
         prioridades.style.borderRadius = '5px';
+        document.getElementById('datosPublicidad').style.display = 'flex';
+        
 }
-
 
 function prioridadAlta() {
         document.getElementById('prioridadBaja').style.border= "none"; 
@@ -25,5 +31,13 @@ function prioridadAlta() {
         let prioridades = document.getElementById('prioridadAlta'); 
         prioridades.style.border = "1px solid #A66FA6";
         prioridades.style.borderRadius = '5px';
-
+        document.getElementById('datosPublicidad').style.display = 'flex';
 }
+
+ultimoInputDatosPublicidad.addEventListener('keydown', function () {
+        document.getElementById('datosPago').style.display = 'flex';
+});
+
+ultimoInputDatosPago.addEventListener('keydown', function() {
+        document.getElementById('enviarDatos').style.opacity = 1;
+});
